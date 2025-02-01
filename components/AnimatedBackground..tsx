e89cd9bceefb4be5b1177601b5c89911cac27276
Mario Oately <davidkalina@proton.me>
2025-02-01 02:53:13 -0700

    style: shrink shapes on mobile

--- components/AnimatedBackground..tsx
+++ components/AnimatedBackground..tsx
@@ -57,7 +57,7 @@ const AnimatedBackground = () => {
   const baseShapes = [
     {
       shape: "circle",
-      className: "w-32 h-32 bg-zinc-500 rounded-full",
+      className: "w-16 h-16 md:w-32 md:h-32 bg-zinc-500 rounded-full",
       animation: "animate-float-slow",
       style: {},
     },
@@ -75,7 +75,7 @@ const AnimatedBackground = () => {
     },
     {
       shape: "triangle",
-      className: "w-32 h-32 bg-zinc-100",
+      className: "w-16 h-16 md:w-32 md:h-32 bg-zinc-100",
       style: { clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)" },
       animation: "animate-float-medium",
     },
